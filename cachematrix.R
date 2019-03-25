9299e3340ff945b5237f52f33ea07380625f7cfb
Laura <lmwilliamson11@gmail.com>
2019-03-25 15:55:04 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -31,18 +31,3 @@ cacheSolve <- function(x, ...) {
     m
 }
 
-test = function(mat){
-    ## @mat: an invertible matrix
-    
-    temp = makeCacheMatrix(mat)
-    
-    start.time = Sys.time()
-    cacheSolve(temp)
-    dur = Sys.time() - start.time
-    print(dur)
-    
-    start.time = Sys.time()
-    cacheSolve(temp)
-    dur = Sys.time() - start.time
-    print(dur)
-}
